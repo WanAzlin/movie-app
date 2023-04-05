e5557f55490953591bc45ef733bb4a4ffc6d08e6
Wan Azlin <wannurulazlin@gmail.com>
2023-04-05 12:35:27 +0800

    hello_wrld

---
+++ movieapp/movieapp/movieappApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieappApp.swift
+//  movieapp
+//
+//  Created by Wan Azlin on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct movieappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
